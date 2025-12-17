f31b0b06326d01dcde7b0871bf666836f6db736e
Gichohi-Simon <njogugichohi56@gmail.com>
2025-12-17 11:07:58 +0300

    font added

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -1,11 +1,9 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen">
+    <div className="min-h-screen">
       <p className="font-inter">admin</p>
       <br />
-      <p className="font-raleway">hello
-        
-      </p>
+      <p className="font-raleway">hello</p>
     </div>
   );
 }
