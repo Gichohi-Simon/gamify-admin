57b25dec9ff61d5acfdcb9e33079f0657839e8f3
Gichohi-Simon <njogugichohi56@gmail.com>
2025-12-20 23:04:40 +0300

    hooks and fetch functions for users done

--- admin/hooks/user.ts
+++ admin/hooks/user.ts
@@ -1,9 +1,75 @@
-import { getAllUsers } from "@/lib/api/user.api";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import {
+  getAllUsers,
+  getBannedUsers,
+  getSingleUserById,
+  banUserFromPlatform,
+  restoreBannedUser,
+  makeUserAnAdmin,
+  revokeUserAdminPriviledge,
+} from "@/lib/api/user.api";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const useGetAllUsers = () => {
   return useQuery({
-    queryKey: ["allUsers"],
+    queryKey: ["users"],
     queryFn: getAllUsers,
+  });
+};
+
+export const useGetSingleUserById = (id: string) => {
+  return useQuery({
+    queryKey: ["user", id],
+    queryFn: () => getSingleUserById(id),
+    enabled: !!id,
+    staleTime: Infinity,
+  });
+};
+
+export const useGetBannedUsers = () => {
+  return useQuery({
+    queryKey: ["bannedUsers"],
+    queryFn: getBannedUsers,
+  });
+};
+
+export const useBanUserFromPlatform = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: banUserFromPlatform,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["users"] });
+      queryClient.invalidateQueries({ queryKey: ["bannedUsers"] });
+    },
+  });
+};
+
+export const useRestoreBannedUser = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: restoreBannedUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["users"] });
+      queryClient.invalidateQueries({ queryKey: ["bannedUsers"] });
+    },
+  });
+};
+
+export const useMakeUserAnAdmin = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: makeUserAnAdmin,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["users"] });
+    },
+  });
+};
+
+export const useRevokeUserAsAdmin = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: revokeUserAdminPriviledge,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["users"] });
+    },
   });
 };
--- admin/lib/api/user.api.ts
+++ admin/lib/api/user.api.ts
@@ -6,6 +6,70 @@ export const getAllUsers = async (): Promise<UserData[]> => {
     credentials: "include",
   });
   if (!response.ok) throw new Error("failed to fetch users");
-  const data = await response.json();
-  return data;
+  const data: { users: UserData[] } = await response.json();
+  return data.users;
+};
+
+export const getSingleUserById = async (id: string): Promise<UserData> => {
+  const response = await fetch(`${API}/users/single-user/${id}`, {
+    credentials: "include",
+  });
+  if (!response.ok) throw new Error("failed to fetch user");
+  const data: { user: UserData } = await response.json();
+  return data.user;
+};
+
+export const getBannedUsers = async (): Promise<UserData[]> => {
+  const response = await fetch(`${API}/users/getDeletedAccounts`, {
+    credentials: "include",
+  });
+  if (!response.ok) throw new Error("failed to fetch banned users");
+  const data: { deletedAccounts: UserData[] } = await response.json();
+  return data.deletedAccounts;
+};
+
+export const banUserFromPlatform = async (id: string): Promise<UserData> => {
+  const response = await fetch(`${API}/users/ban-user-from-platform/${id}`, {
+    method: "PATCH",
+    credentials: "include",
+    headers: { "Content-Type": "application/json" },
+  });
+  if (!response.ok) throw new Error("failed to ban user");
+  const data: { deletedUser: UserData } = await response.json();
+  return data.deletedUser;
+};
+
+export const restoreBannedUser = async (id: string): Promise<UserData> => {
+  const response = await fetch(`${API}/users/ban-user-from-platform/${id}`, {
+    method: "PATCH",
+    credentials: "include",
+    headers: { "Content-Type": "application/json" },
+  });
+  if (!response.ok) throw new Error("failed to restore user");
+  const data: { restoredAccount: UserData } = await response.json();
+  return data.restoredAccount;
+};
+
+export const makeUserAnAdmin = async (id: string): Promise<UserData> => {
+  const response = await fetch(`${API}/users/makeAdmin/${id}`, {
+    method: "PATCH",
+    credentials: "include",
+    headers: { "Content-Type": "application/json" },
+  });
+  if (!response.ok) throw new Error("failed to make user an admin");
+  const data: { user: UserData } = await response.json();
+  return data.user;
+};
+
+export const revokeUserAdminPriviledge = async (
+  id: string,
+): Promise<UserData> => {
+  const response = await fetch(`${API}/users/removeAdmin/${id}`, {
+    method: "PATCH",
+    credentials: "include",
+    headers: { "Content-Type": "application/json" },
+  });
+  if (!response.ok) throw new Error("failed to remove user as an admin");
+  const data: { user: UserData } = await response.json();
+  return data.user;
 };
