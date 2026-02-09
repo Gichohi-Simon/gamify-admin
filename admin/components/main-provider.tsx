71c05bb51f75bd77ebcc80c484a797a5cb6162bd
Gichohi-Simon <njogugichohi56@gmail.com>
2026-02-09 16:07:20 +0300

    removed all google related code

--- admin/components/main-provider.tsx
+++ admin/components/main-provider.tsx
@@ -6,9 +6,6 @@ import { store } from "../store/store";
 import QueryClientComponent from "@/components/query-client-component";
 import { Toaster } from "./ui/sonner";
 import AuthProvider from "./auth-provider";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-
-const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
 
 export default function MainProvider({
   children,
@@ -17,13 +14,11 @@ export default function MainProvider({
 }) {
   return (
     <Provider store={store}>
-      <GoogleOAuthProvider clientId={googleClientId!}>
-        <QueryClientComponent>
-          <Toaster position="top-right" richColors closeButton />
-          <AuthProvider />
-          {children}
-        </QueryClientComponent>
-      </GoogleOAuthProvider>
+      <QueryClientComponent>
+        <Toaster position="top-right" richColors closeButton />
+        <AuthProvider />
+        {children}
+      </QueryClientComponent>
     </Provider>
   );
 }
